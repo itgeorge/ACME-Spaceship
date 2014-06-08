577e20e212ab98d427628a8053344e8d7f9ae5bf
itgeorge <georgi.stef.georgiev@gmail.com>
2014-06-08 20:16:53 +0300

    Started initial "main.js" code

--- ACME-Spaceship/Ui/main.js
+++ ACME-Spaceship/Ui/main.js
@@ -1,1 +1,11 @@
-﻿
+﻿function init() {
+    var canvasEl = document.getElementById("viewport.js");
+    var canvasContext = canvas.getContext("2d");
+
+    //Initialize renderer by canvas context
+    //Initialize engine by renderer and attach ui events
+}
+
+function onKeyDown(event) {
+    
+}
