e87e45732c1c5c66a0e1eb4cf85b9a92c4fb6a8d
ivivanov <me@ivivanov.com>
2014-06-08 20:40:53 +0300

    keyboard events

--- ACME-Spaceship/Ui/main.js
+++ ACME-Spaceship/Ui/main.js
@@ -1,11 +1,36 @@
 ﻿function init() {
+    //CONST
+    var screenWidth = 800;
+    var screenHeight = 640;
+
+    //INIT
     var canvasEl = document.getElementById("viewport.js");
     var canvasContext = canvas.getContext("2d");
+    var engine = new Engine(screenWidth, screenHeight);
 
-    //Initialize renderer by canvas context
-    //Initialize engine by renderer and attach ui events
-}
+    //EVENTS
+    window.addEventListener("keydown", onKeyDown, false);
 
-function onKeyDown(event) {
-    
+    //FUNCTIONS
+    function onKeyDown(event) {
+        log(event.keyCode);
+        switch (event.keyCode) {
+            case 37:
+                // left key pressed
+                break;
+            case 38:
+                // up key pressed
+                break;
+            case 39:
+                // right key pressed
+                break;
+            case 40:
+                // down key pressed
+                break;
+        }
+    }
+
+    function log(msg) {
+        console.log(msg);
+    }
 }
