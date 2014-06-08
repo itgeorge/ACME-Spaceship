738c7987fe0370eb17fd75e2da61ebe77efc26f4
itgeorge <georgi.stef.georgiev@gmail.com>
2014-06-08 22:53:58 +0300

    Added spawn logic, expecting gameLogic object

--- ACME-Spaceship/Scene/engine.js
+++ ACME-Spaceship/Scene/engine.js
@@ -1,5 +1,10 @@
 ﻿var Engine = Class.create({    
-    initialize: function() {
-        
-    } 
+    initialize: function(gameLogic) {
+        this.gameLogic = gameLogic;
+        this.enemies = [];
+    },
+    spawnEnemies: function spawnEnemies() {
+        var newEnemies = this.gameLogic.getNewEnemies(this.enemies);
+        this.enemies.push.apply(this.enemies, newEnemies);
+    }
 });
