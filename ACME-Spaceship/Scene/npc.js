7c5e9d9e35392e2fccc32afc5930ab09ac19fce5
itgeorge <georgi.stef.georgiev@gmail.com>
2014-06-10 00:07:05 +0300

    Added constructor

--- ACME-Spaceship/Scene/npc.js
+++ ACME-Spaceship/Scene/npc.js
@@ -3,5 +3,10 @@ Scene.NPC = Class.create(Scene.SelfControlledObject, {
     initialize: function ($super, firing, movement, x, y, radius, hitpoints, maxSpeed, type, renderType) {
         $super(movement, x, y, radius, hitpoints, maxSpeed, type, renderType);
         this.firing = firing;
+    },
+    update: function ($super) {
+        var firedObject = this.firing.getFiredObject();
+        this._addProduced(firedObject);
+        return $super();
     }
 });
